7f5cacf530b38b48ff221af8c44b3f431c5f00a1
Alma <isakvent@outlook.com>
2024-08-08 21:19:30 +0200

    create a page turner in the pilot page because infinite scroll does not work

--- src/pages/home_page/Piloting.js
+++ src/pages/home_page/Piloting.js
@@ -3,43 +3,63 @@ import React, { useEffect, useState } from "react";
 import Styles from "../../styles/Piloting.module.css";
 import "../../global.css";
 import { axiosReq } from "../../api/axiosDefaults";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { fetchMoreData } from "../../utils/utils";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import axios from "axios";
 
 const Piloting = ({ filter = ""}) => {
     // eslint-disable-next-line
     const [projects, setProjects] = useState({ results: [] });
         // eslint-disable-next-line
     const [query, setQuery] = useState("");
+    const { pathname } = useLocation();
 
-    const fetchProjects = async () => {
-        try {
-            const { data } = await axiosReq.get(`/projects`);
-            setProjects(data);
-        } catch (err) {
+    const [page, setPage] = useState(1);
+    const [nextPage, setNextPage] = useState(true);
+    const [previousPage, setPreviousPage] = useState(true);
 
-        }
-    };
+    const handleGetNext = (event) => {
+        event.preventDefault();
+        setPage(page + 1);
+    }
+    const handleGetPrevious = (event) => {
+        event.preventDefault();
+        setPage(page - 1);
+    }
 
     useEffect(() => {
+        const fetchProjects = async () => {
+            try {
+                const { data } = await axiosReq.get(`/projects/?page=${page}`);
+                setProjects(data);
+
+                setNextPage(data.next !== null);
+                setPreviousPage(data.previous !== null);
+            } catch (err) {
+    
+            }
+        };
         fetchProjects();
-    }, [filter, query]);
+    }, [filter, query, pathname, page]);
 
     return(<div className={Styles.PilotingContainer}>
         <hr/>
         <p>found: {projects.results.length} items</p>
 
-        <InfiniteScroll
-        dataLength={projects.results.length}
-        loader={<p>Loading...</p>}
-        hasMore={!!projects.next}
-        next={() => fetchMoreData(projects, setProjects)}
-        >
-            {projects.results.map((post) => (<>
-            <hr/>
-            <p>item {post?.id}</p>
-            </>))}
-        </InfiniteScroll>
+        {projects.results.length ? (
+            projects.results.map((project) => (<>
+                <hr/>
+                <p>item: {project?.id}</p>
+            </>))
+        ) : null }
+
+    <p>current page: {page}</p>
+
+    {previousPage ? (<>
+        <button onClick={handleGetPrevious}>previous</button>
+    </>) : null}
+    {nextPage ? (<>
+        <button onClick={handleGetNext}>next</button>
+    </>) : null}
 
     </div>)
 };
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,21 +2,21 @@
 import { axiosReq } from "../api/axiosDefaults";
 
 export const fetchMoreData = async (resource, setResource) => {
-    try{
-        const {data} = await axiosReq.get(resource.next);
-        setResource((prevResource) => ({
-            ...prevResource,
-            nenxt:data.next,
-            results: data.results.reduce((acc, cur) => {
-                return acc.some(accResult => accResult.id === cur.id )
-                ? acc
-                : [...acc, cur];
-            }, prevResource.results),
-        }));
-    } catch(err){
-
+    try {
+      const { data } = await axiosReq.get(resource.next);
+      setResource((prevResource) => ({
+        ...prevResource,
+        next: data.next,
+        results: data.results.reduce((acc, cur) => {
+          return acc.some((accResult) => accResult.id === cur.id)
+            ? acc
+            : [...acc, cur];
+        }, prevResource.results),
+      }));
+    } catch (err) {
+        
     }
-};
+  };
 
 /*
 export const setTokenTimeStamp = (data) => {
