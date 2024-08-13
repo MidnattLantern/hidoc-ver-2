4b1e8ab7261df82aedb1bc5059da9e4b1b6d3c4e
Alma <isakvent@outlook.com>
2024-08-13 18:46:23 +0200

    switch axios base url for beta deployment

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//axios.defaults.baseURL = 'https://hidoc-api-80e680483d64.herokuapp.com/'; // for deployment
-axios.defaults.baseURL = 'http://127.0.0.1:8000/'; // for local development
+axios.defaults.baseURL = 'https://hidoc-api-80e680483d64.herokuapp.com/'; // for deployment
+//axios.defaults.baseURL = 'http://127.0.0.1:8000/'; // for local development
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
